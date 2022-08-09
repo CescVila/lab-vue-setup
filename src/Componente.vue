444ecc62585763c1cecea083c98ff0bb46de9736
CescVila <vr.cesc@gmail.com>
2022-08-09 16:55:09 +0200

    lab day 1 week 4 done

---
+++ src/Componente.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>
+      Hola mi nombre es{{ name }}, soy de {{ place }} y tengo {{ years }} años
+    </h1>
+  </div>
+  <!-- <template v-if="display">
+    <h1>Puedes tomar alcohol</h1>
+    <template v-if="display" >
+        <h1>No puedes tomar</h1>
+        <template>
+            <h1>Estás muy viejo para tomar</h1>
+        </template>
+    </template>
+
+  </template> -->
+</template>
+
+<script>
+export default {
+  setup() {
+    return{
+
+    name: "Cesc"
+    place: "BCN"
+    years: "29"
+
+  },
+  }
+
+};
+</script>
+
+<style>
+.color-blue {
+  background-color: blue;
+}
+.h1{
+    font-size: 18px;
+}
+</style>
---
+++ src/components/CescCom.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <h1>Hello World</h1>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    
+
+    };
+  },
+  methods: {
+
+  
+    },
+  }
+
+  // name: 'Cesc',
+  // inyeccion:`<h1>Hola TEXTO</h1>`,
+  // myAge: `29`,
+  // broName: `Uri`,
+  // broAge: `26`,
+  // familyName: `Vilà`,
+  // familyAge: `50`,
+  // rainbow:'#000'
+
+</script>
+
+<style>
+
+</style>
